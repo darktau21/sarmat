9fcca9a8828338550f25384061c314ce1459a787
Kirill Naumenko <naumenkokirill466@gmail.com>
2021-12-18 04:49:02 +0300

    Более точное отслеживание клика (в т.ч. и вне области меню)

--- src/includes/header/header.js
+++ src/includes/header/header.js
@@ -1,8 +1,23 @@
-console.log('Приветствую смотрящих!')
-let menuBtn = document.querySelector('.main-menu__mobile-button')
-let menuBody = document.querySelector('.main-menu__body')
+console.log('Приветствую смотрящих!');
+let menuBtn = document.querySelector('.main-menu__mobile-button');
+let menuBody = document.querySelector('.main-menu__body');
 
-menuBtn.addEventListener('click', function(){
-  menuBtn.classList.toggle('main-menu__mobile-button_active')
-  menuBody.classList.toggle('main-menu__body_active')
-})
+const toggleMenu = function () {
+  menuBtn.classList.toggle('main-menu__mobile-button_active');
+  menuBody.classList.toggle('main-menu__body_active');
+};
+
+menuBtn.addEventListener('click', function (e) {
+  toggleMenu();
+});
+
+document.addEventListener('click', function (e) {
+  const target = e.target;
+  const its_menuBody = target == menuBody || menuBody.contains(target);
+  const its_menuBtn = target == menuBtn;
+  const menu_is_active = menuBody.classList.contains('main-menu__body_active');
+
+  if (!its_menuBody && !its_menuBtn && menu_is_active) {
+    toggleMenu();
+  }
+});
