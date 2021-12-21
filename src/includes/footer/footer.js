d531e548a2f8b8e62faa086b2ee1eee873cbd739
Kirill Naumenko <naumenkokirill466@gmail.com>
2021-12-21 06:38:32 +0300

    Пофикшен краш JS + содержимое вынесено в конфиг

--- src/includes/footer/footer.js
+++ src/includes/footer/footer.js
@@ -1,6 +1,3 @@
 let currentDate = new Date();
-let copyright = document.createElement('div');
-copyright.className = 'footer__copyright';
-copyright.innerHTML = '© Southern Federal, ' + currentDate.getFullYear();
-
-document.querySelector('.footer__info').append(copyright);
+let copyright = document.querySelector('.footer__copyright');
+copyright.textContent += currentDate.getFullYear();
