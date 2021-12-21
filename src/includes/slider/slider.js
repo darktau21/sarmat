ee28e4bfe4b7a2fffd28aa01feb34cd0e0e2a1cc
Kirill Naumenko <naumenkokirill466@gmail.com>
2021-12-21 06:39:03 +0300

    Слайдер почти доведен до идеала

--- src/includes/slider/slider.js
+++ src/includes/slider/slider.js
@@ -39,8 +39,8 @@ let slides = [
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-let currentSlideNum = getRandom(0, slides.length - 1)
+let slidesMassLenght = slides.length - 1;
+let currentSlideNum = getRandom(0, slidesMassLenght);
 let currentSlide = slides[currentSlideNum];
 
 function changeSlide() {
@@ -53,25 +53,46 @@ function changeSlide() {
   document.querySelector('.slider__title').textContent = currentSlide.title;
   document.querySelector('.slider__text').textContent = currentSlide.text;
   document
-    .querySelector('#slider__to-article-mobile')
+    .querySelector('#slider__to-article')
     .setAttribute('href', currentSlide.link);
 }
 
 changeSlide();
 
-function checkSlide() {
+function checkNextSlide() {
   currentSlideNum += 1;
   currentSlideNum = currentSlideNum == slides.length ? 0 : currentSlideNum;
-  currentSlide = slides[currentSlideNum]
+  currentSlide = slides[currentSlideNum];
+  changeSlide();
 }
 
-let nextSlideBtn = document.querySelector('.slider__next-slide')
+function checkPrevSlide() {
+  currentSlideNum -= 1;
+  currentSlideNum = currentSlideNum == -1 ? slidesMassLenght : currentSlideNum;
+  currentSlide = slides[currentSlideNum];
+  changeSlide();
+}
+
+let nextSlideBtn = document.querySelector('.slider__slide-btn_next');
 document.addEventListener('click', function (e) {
   let target = e.target;
-  let its_nextSlideBtn = target == nextSlideBtn || nextSlideBtn.contains(target);
+  let its_nextSlideBtn =
+    target == nextSlideBtn || nextSlideBtn.contains(target);
 
   if (its_nextSlideBtn) {
-    checkSlide()
-    changeSlide()
+    checkNextSlide();
   }
 });
+
+let prevSlideBtn = document.querySelector('.slider__slide-btn_prev');
+document.addEventListener('click', function (e) {
+  let target = e.target;
+  let its_prevSlideBtn =
+    target == prevSlideBtn || prevSlideBtn.contains(target);
+
+  if (its_prevSlideBtn) {
+    checkPrevSlide();
+  }
+});
+
+
