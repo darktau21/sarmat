1b1f6c4c19d9c6ad85390799c313c8ab86c790d2
Kirill Naumenko <naumenkokirill466@gmail.com>
2021-12-27 06:02:57 +0300

    Рефакторинг, фикс подгрузки изображений

--- src/includes/slider/slider.js
+++ src/includes/slider/slider.js
@@ -1,94 +1,67 @@
-function getRandom(min, max) {
+let slidesMassLenght = slides.length - 1; // Количество слайдов
+let currentSlideNum = getRandom(0, slidesMassLenght); // Номер текущего слайда
+let nextSlideNum; // Номер следующего слайда
+let prevSlideNum; // Номер предыдущего слайда
+let currentSlide = slides[currentSlideNum]; // Объект с текущим слайдом
+
+// Кнопки
+let nextSlideBtn = document.querySelector('.slider__slide-btn_next'); // Кнопка вперед ПК
+let nextSlideBtnMobile = document.querySelector('.slider__slide-btn-mobile_next'); // Кнопка вперед мобилки
+let prevSlideBtn = document.querySelector('.slider__slide-btn_prev'); // Кнопка назад ПК
+let prevSlideBtnMobile = document.querySelector('.slider__slide-btn-mobile_prev'); // Кнопка назад мобилки
+
+
+function checkSlideNum(which) { // Проверка краёв массива, true - возвращает следующий, false - предыдущий
+  nextSlideNum = currentSlideNum == slidesMassLenght ? 0 : currentSlideNum + 1; // Номер следующего слайда
+  prevSlideNum = currentSlideNum == 0 ? slidesMassLenght : currentSlideNum - 1; // Номер предыдущего слайда
+  currentSlideNum = which ? nextSlideNum : prevSlideNum;
+  currentSlide = slides[currentSlideNum];
+  console.log(currentSlideNum);
+}
+
+function getRandom(min, max) { // Функция рандома
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function preloadImages() {
+
+function preloadImages() { // Предзагрузка изображений
   for (var i = 0; i < arguments.length; i++) {
     new Image().src = arguments[i];
+    console.log('Загрузка изображений', (arguments.length))
   }
 }
-let slidesMassLenght = slides.length - 1;
-let currentSlideNum = getRandom(0, slidesMassLenght);
-let currentSlide = slides[currentSlideNum];
 
-function changeSlide() {
-  document
-    .querySelector('#slider__picture')
-    .setAttribute('src', currentSlide.picture);
-  document
-    .querySelector('#slider__picture')
-    .setAttribute('alt', currentSlide.alt);
+function changeSlide() { // Непосредственная смена контента в слайдере
+  document.querySelector('#slider__picture').setAttribute('src', currentSlide.picture);
+  document.querySelector('#slider__picture').setAttribute('alt', currentSlide.alt);
   document.querySelector('.slider__title').textContent = currentSlide.title;
   document.querySelector('.slider__text').textContent = currentSlide.text;
-  document
-    .querySelector('#slider__to-article')
-    .setAttribute('href', currentSlide.link);
-  preloadImages(
-    slides[currentSlideNum + 1].picture,
-    slides[currentSlideNum - 1].picture
-  );
-  console.log(slides[currentSlideNum + 1].picture, slides[currentSlideNum - 1].picture)
+  document.querySelector('#slider__to-article').setAttribute('href', currentSlide.link);
+  preloadImages(slides[nextSlideNum].picture, slides[prevSlideNum].picture);
 }
 
-changeSlide();
-
-function checkNextSlide() {
-  currentSlideNum += 1;
-  currentSlideNum = currentSlideNum == slides.length ? 0 : currentSlideNum;
-  currentSlide = slides[currentSlideNum];
+// Функции прослушивания кнопок
+nextSlideBtn.addEventListener('click', function (e) {
+  checkSlideNum(true);
   changeSlide();
-}
-
-function checkPrevSlide() {
-  currentSlideNum -= 1;
-  currentSlideNum = currentSlideNum == -1 ? slidesMassLenght : currentSlideNum;
-  currentSlide = slides[currentSlideNum];
+  console.log('Следующий ПК')
+});
+prevSlideBtn.addEventListener('click', function (e) {
+  checkSlideNum(false);
   changeSlide();
-}
-
-let nextSlideBtn = document.querySelector('.slider__slide-btn_next');
-document.addEventListener('click', function (e) {
-  let target = e.target;
-  let its_nextSlideBtn =
-    target == nextSlideBtn || nextSlideBtn.contains(target);
-
-  if (its_nextSlideBtn) {
-    checkNextSlide();
-  }
+  console.log('Предыдущий ПК')
+});
+nextSlideBtnMobile.addEventListener('click', function (e) {
+  checkSlideNum(true);
+  changeSlide();
+  console.log('Следующий мобила')
+});
+prevSlideBtnMobile.addEventListener('click', function (e) {
+  checkSlideNum(false);
+  changeSlide();
+  console.log('Предыдущий мобила')
 });
 
-let prevSlideBtn = document.querySelector('.slider__slide-btn_prev');
-document.addEventListener('click', function (e) {
-  let target = e.target;
-  let its_prevSlideBtn =
-    target == prevSlideBtn || prevSlideBtn.contains(target);
-
-  if (its_prevSlideBtn) {
-    checkPrevSlide();
-  }
-});
-
-let nextSlideBtnMobile = document.querySelector(
-  '.slider__slide-btn-mobile_next'
-);
-document.addEventListener('click', function (e) {
-  let target = e.target;
-  let its_nextSlideBtnMobile =
-    target == nextSlideBtnMobile || nextSlideBtnMobile.contains(target);
-
-  if (its_nextSlideBtnMobile) {
-    checkNextSlide();
-  }
-});
-
-let prevSlideBtnMobile = document.querySelector(
-  '.slider__slide-btn-mobile_prev'
-);
-document.addEventListener('click', function (e) {
-  let target = e.target;
-  let its_prevSlideBtnMobile =
-    target == prevSlideBtnMobile || prevSlideBtnMobile.contains(target);
-
-  if (its_prevSlideBtnMobile) {
-    checkPrevSlide();
-  }
-});
+window.onload = function () {
+  checkSlideNum();
+  changeSlide(); // После полной загрузки вызываем функцию
+};
