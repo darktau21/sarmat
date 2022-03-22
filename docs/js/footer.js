25e33590bce4ecc309477c8d5ce2e4a1042406f8
Kirill Naumenko <naumenkokirill466@gmail.com>
2022-03-22 05:05:28 +0300

    для github pages

---
+++ docs/js/footer.js
@@ -0,0 +1,1 @@
+"use strict";const copyrightYear=document.querySelector(".js_copyright-year");window.addEventListener("DOMContentLoaded",(()=>{let e=new Date;copyrightYear.textContent=e.getFullYear()}),{once:!0});
